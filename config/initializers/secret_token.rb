fb5008810d68ebf5108553687469410298f1069a
Ashvin Nair <nair.ashvin@gmail.com>
2014-01-24 23:39:28 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catan::Application.config.secret_token = '1b6ba1c2e74ca61c7ab532090e308c71a197010266bb335209b48ba4097866afee3c47af25f7522aefa4b66df2a7a3f6656a3affe9a5e25cd59acc0c78bf2d75'
