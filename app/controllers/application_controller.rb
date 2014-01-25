b2637f5dd83fbad2e3f864c4c97c11ad59792644
Ashvin Nair <nair.ashvin@gmail.com>
2014-01-25 05:42:52 -0800

    trying to allow access to the music file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
 
   after_filter :set_access_control_headers
 
-    def set_access_control_headers 
-    headers['Access-Control-Allow-Origin'] = 'http://localhost:8081/' 
-    headers['Access-Control-Request-Method'] = '*' 
+    def set_access_control_headers
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Request-Method'] = '*'
     end
 end
