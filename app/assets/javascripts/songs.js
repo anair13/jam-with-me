d8cb338f065022e101adbe98ec009bd6ad7c59f3
unknown <saavan@berkeley.edu>
2014-01-26 06:25:24 -0800

    slowly lowering standards

--- app/assets/javascripts/songs.js
+++ app/assets/javascripts/songs.js
@@ -111,11 +111,6 @@ function init() {
         setInterval(function () {
             var thisDate = new Date();
             $('#timer').text(get_elapsed_time_string(Math.floor((endTime - thisDate.getTime()) / 1000)));}, 1000);
-            console.log(endTime - thisDate.getTime());
-            if(endTime < thisDate.getTime()){
-                alert("this is a problem!");
-                $('#error').text('<p style="color:red">This Session has expired</p>');
-            }
     });
 
     myDataRef = new Firebase(firebase_song_identifier);
