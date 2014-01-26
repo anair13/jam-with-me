f50a887b733f47aa1435d0043a9ee48da5ae46f2
Ashvin Nair <nair.ashvin@gmail.com>
2014-01-25 16:02:24 -0800

    Moved ugly js to assets from inline html Modified routes for better default interaction

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Catan::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'songs#index'
 
   # See how all your routes lay out with "rake routes"
 
